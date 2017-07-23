fff8a630e2eaf1c87afe6d9dcaa67f574ed6b8bd
chpchen <chpchen@163.com>
2017-07-23 14:49:28 +0800

    ARouter.getInstance().inject(this);//注解时传参加

--- app/src/main/java/com/example/arouterdemo/ui/image/ImageActivity.java
+++ app/src/main/java/com/example/arouterdemo/ui/image/ImageActivity.java
@@ -6,7 +6,9 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.bumptech.glide.Glide;
 import com.example.arouterdemo.R;
 import com.example.basecommonlibrary.RouterCommonUtil;
@@ -21,11 +23,19 @@ import com.example.basecommonlibrary.RouterCommonUtil;
 
 @Route(path = "/ui/image", group = "图片")
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Autowired
+    public String arg1;
+
+    @Autowired(name = "arg1")
+    public String argx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
         findViewById(R.id.bt1).setOnClickListener(this);
+        ARouter.getInstance().inject(this);
         String url = getIntent().getStringExtra("arg1");
         if (!TextUtils.isEmpty(url)) {
             Glide.with(this).load(url).into(((ImageView) findViewById(R.id.imageView)));
